52427480ab34347f022e856b75e316771cd9c665
snehalchavann <78457966+snehalchavann@users.noreply.github.com>
2021-02-08 22:02:22 -0500

    Lab 1 updates

---
+++ lab1/src/model/product.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author snehalchavan
+ */
+public class product {
+    
+    private String name;
+    private String desc;
+    private String price;
+    private String quantity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
+
+    public String getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(String quantity) {
+        this.quantity = quantity;
+    }
+    
+    
+    
+}
