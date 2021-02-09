7d1491ba6547f2275828910a2a070f978a2d7464
snehalchavann <78457966+snehalchavann@users.noreply.github.com>
2021-02-08 22:26:55 -0500

    Assignment1 Project

---
+++ Assignments/Assignment1/src/Model/Bank.java
@@ -0,0 +1,146 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author snehalchavan
+ */
+public class Bank {
+    
+    String bnamesave;
+
+    public String getBnamesave() {
+        return bnamesave;
+    }
+
+    public void setBnamesave(String bnamesave) {
+        this.bnamesave = bnamesave;
+    }
+
+    public String getAccountnosave() {
+        return accountnosave;
+    }
+
+    public void setAccountnosave(String accountnosave) {
+        this.accountnosave = accountnosave;
+    }
+
+    public String getRoutingnosave() {
+        return routingnosave;
+    }
+
+    public void setRoutingnosave(String routingnosave) {
+        this.routingnosave = routingnosave;
+    }
+
+    public String getBalancesave() {
+        return balancesave;
+    }
+
+    public void setBalancesave(String balancesave) {
+        this.balancesave = balancesave;
+    }
+
+    public String getAcctypesave() {
+        return acctypesave;
+    }
+
+    public void setAcctypesave(String acctypesave) {
+        this.acctypesave = acctypesave;
+    }
+
+    public String getBnamecheck() {
+        return bnamecheck;
+    }
+
+    public void setBnamecheck(String bnamecheck) {
+        this.bnamecheck = bnamecheck;
+    }
+
+    public String getAccountnocheck() {
+        return accountnocheck;
+    }
+
+    public void setAccountnocheck(String accountnocheck) {
+        this.accountnocheck = accountnocheck;
+    }
+
+    public String getRoutingnocheck() {
+        return routingnocheck;
+    }
+
+    public void setRoutingnocheck(String routingnocheck) {
+        this.routingnocheck = routingnocheck;
+    }
+
+    public String getBalancecheck() {
+        return balancecheck;
+    }
+
+    public void setBalancecheck(String balancecheck) {
+        this.balancecheck = balancecheck;
+    }
+
+    public String getAcctypecheck() {
+        return acctypecheck;
+    }
+
+    public void setAcctypecheck(String acctypecheck) {
+        this.acctypecheck = acctypecheck;
+    }
+    String accountnosave;
+    String routingnosave;
+    String balancesave;
+    String acctypesave;
+    
+    String bnamecheck;
+    String accountnocheck;
+    String routingnocheck;
+    String balancecheck;
+    String acctypecheck;
+
+//    public String getBname() {
+//        return bname;
+//    }
+//
+//    public void setBname(String bname) {
+//        this.bname = bname;
+//    }
+//
+//    public String getAccountno() {
+//        return accountno;
+//    }
+//
+//    public void setAccountno(String accountno) {
+//        this.accountno = accountno;
+//    }
+//
+//    public String getRoutingno() {
+//        return routingno;
+//    }
+//
+//    public void setRoutingno(String routingno) {
+//        this.routingno = routingno;
+//    }
+//
+//    public String getBalance() {
+//        return balance;
+//    }
+//
+//    public void setBalance(String balance) {
+//        this.balance = balance;
+//    }
+//
+//    public String getAcctype() {
+//        return acctype;
+//    }
+//
+//    public void setAcctype(String acctype) {
+//        this.acctype = acctype;
+//    }
+//    
+}
---
+++ Assignments/Assignment1/src/Model/Person.java
@@ -0,0 +1,214 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Model;
+
+/**
+ *
+ * @author snehalchavan
+ */
+public class Person {
+    String firstname;
+    String lastname;
+    String dob;
+    String age;
+    String height;
+    String weight;
+    String phoneno;
+    String ssn;  
+    String street;
+    String city;
+    String state;
+    String zipcode;
+    String licenseno;
+    String dtofissue;
+    String dtofexpire;
+    String bloodtype;
+    String recordno;
+    String allergy1;
+    String allergy2;
+    String allergy3;
+    Bank savingaccount;
+    Bank checkinaccount;
+
+    public String getRecordno() {
+        return recordno;
+    }
+
+    public void setRecordno(String recordno) {
+        this.recordno = recordno;
+    }
+
+    public String getAllergy1() {
+        return allergy1;
+    }
+
+    public void setAllergy1(String allergy1) {
+        this.allergy1 = allergy1;
+    }
+
+    public String getAllergy2() {
+        return allergy2;
+    }
+
+    public void setAllergy2(String allergy2) {
+        this.allergy2 = allergy2;
+    }
+
+    public String getAllergy3() {
+        return allergy3;
+    }
+
+    public void setAllergy3(String allergy3) {
+        this.allergy3 = allergy3;
+    }
+    
+
+    public String getLicenseno() {
+        return licenseno;
+    }
+
+    public void setLicenseno(String licenseno) {
+        this.licenseno = licenseno;
+    }
+
+    public String getDtofissue() {
+        return dtofissue;
+    }
+
+    public void setDtofissue(String dtofissue) {
+        this.dtofissue = dtofissue;
+    }
+
+    public String getDtofexpire() {
+        return dtofexpire;
+    }
+
+    public void setDtofexpire(String dtofexpire) {
+        this.dtofexpire = dtofexpire;
+    }
+
+    public String getBloodtype() {
+        return bloodtype;
+    }
+
+    public void setBloodtype(String bloodtype) {
+        this.bloodtype = bloodtype;
+    }
+    
+
+    public Bank getSavingaccount() {
+        return savingaccount;
+    }
+
+    public void setSavingaccount(Bank savingaccount) {
+        this.savingaccount = savingaccount;
+    }
+
+    public Bank getCheckinaccount() {
+        return checkinaccount;
+    }
+
+    public void setCheckinaccount(Bank checkinaccount) {
+        this.checkinaccount = checkinaccount;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+   
+
+     public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getHeight() {
+        return height;
+    }
+
+    public void setHeight(String height) {
+        this.height = height;
+    }
+
+    public String getWeight() {
+        return weight;
+    }
+
+    public void setWeight(String weight) {
+        this.weight = weight;
+    }
+
+    public String getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(String phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+}
